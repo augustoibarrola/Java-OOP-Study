83a0cbe94e6dc7439637417bef1f9531d2cdfb72
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-09-21 20:55:48 -0400

    simple refactor

--- RunTimePolymoraphism/src/Test.java
+++ RunTimePolymoraphism/src/Test.java
@@ -1,34 +1,34 @@
-class Animal {
+class A {
     // most super class method
     public void eat(){
-        System.out.println("Animal eats food");
+        System.out.println("A eats food");
     }
 }
 
-class Dog extends Animal{
+class B extends A{
     // dynamic method dispatch happens to this method
     public void eat() {
-        System.out.println("Dog eats meat");
+        System.out.println("B eats meat");
     }
 }
 
-class Puppy extends Dog{
+class C extends B{
     // dynamic method dispatch happens to this method
     public void eat() {
-        System.out.println("Puppy drinks milk");
+        System.out.println("C drinks milk");
     }
 }
 
 class Test{
     public static void main(String args[]){
         // create reference variables
-        Animal a1, a2, a3;
+        A a1, a2, a3;
         System.out.println("-----No Dynamic method dispatch-----");
-        a1 = new Animal();
+        a1 = new A();
         a1.eat();
         System.out.println("-----Dynamic method dispatch-----");
-        a2 = new Dog();
-        a3 = new Puppy();
+        a2 = new B();
+        a3 = new C();
 
         a2.eat();
         a3.eat();
